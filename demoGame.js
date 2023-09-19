66aa7685f68f91fda56d66e5fa9fef5b194a81e4
Kenjiro-Ocampo <ko88@njit.edu>
2023-09-19 01:15:30 -0400

    Removed Async bug behavior

--- demoGame.js
+++ demoGame.js
@@ -12,8 +12,7 @@ const GameState = {
     InGreen: 2,
     End: 3
 }
-
-// User gen
+//User gen
 //const CurrentUser = new UserPlayer();
 let User = GameState;
 User.GameState = 0;
@@ -23,37 +22,39 @@ const btn = document.querySelector(".btn");
 const returnTime = document.querySelector(".ReturnTime"); 
 // -----
 
-// ====Game Logic====
+
 btn.addEventListener("click", TimerGame); // could move to a lambda func
 
 
+var userGreenTime;
 
 function TimerGame() {
-    switch (User.GameState) {
-        case 0:
-            btn.style.backgroundColor = "red";
-            User.GameState = 1;
-
-            //Pick a number between 1-3 and sec -> milsec
-            var randTime = Math.floor((Math.random() * randTime)+1) * 1000;
-            console.log(randTime);
-            setTimeout(() => {
-                User.GameState = 2;
-                btn.style.backgroundColor = "green";
-                userGreenTime = Date.now();
-            }, randTime);
-            break;
-
-        case 2: //Current state if the user has clicked only in the green
-            var userInputTime = Date.now() - userGreenTime;
-
-            btn.style.backgroundColor = "white";
-            returnTime.innerText = userInputTime/1000;
-            User.GameState = 0; // end or start again havent decided.
-            break;
-
-        default:
-            returnTime.innerText = "Error: Game Defaulted";
-            break;
+    
+    //Set to starting a state transition to InProgress
+    if (User.GameState == 0) {
+        btn.style.backgroundColor = "red";
+        User.GameState = 1;
     }
+    
+    //No cheating protection yet (you can just spam the button rn)
+    if (User.GameState == 1) {
+        //Pick a number between 1-3 and sec -> milsec
+        var randTime = Math.floor((Math.random() * 3)+1) * 1000;
+        console.log(randTime);
+        setTimeout(() => {
+            User.GameState = 2;
+            btn.style.backgroundColor = "green";
+            userGreenTime = Date.now();
+        }, randTime);
+    }
+
+    if (User.GameState == 2) {
+        //Current state if the user has clicked only in the green
+        var userInputTime = Date.now() - userGreenTime;
+
+        btn.style.backgroundColor = "white";
+        returnTime.innerText = userInputTime/1000;
+        User.GameState = 0; // end or start again havent decided.
+    }
+
 }
